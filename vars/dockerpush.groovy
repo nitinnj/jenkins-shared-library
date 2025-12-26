a9d03099ba72ba7682ab330ea8fca6e80ab51cdb
nitinnj <132352386+nitinnj@users.noreply.github.com>
2025-12-26 18:37:19 +0530

    Add Docker push functionality in dockerpush.groovy

---
+++ vars/dockerpush.groovy
@@ -0,0 +1,21 @@
+def call(String credsId, String imageName, String tag) {
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: credsId,
+            usernameVariable: 'DOCKER_USER',
+            passwordVariable: 'DOCKER_PASS'
+        )
+    ]) {
+        sh """
+            echo "Logging in to Docker Hub..."
+            docker login -u $DOCKER_USER -p $DOCKER_PASS
+
+            echo "Pushing image..."
+            docker tag ${imageName}:${tag} $DOCKER_USER/${imageName}:${tag}
+            docker push $DOCKER_USER/${imageName}:${tag}
+
+            echo "Docker push completed"
+        """
+    }
+}
