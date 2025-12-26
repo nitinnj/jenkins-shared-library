4f891f87a1d9b46f49992425ceafcff2d1479f23
nitinnj <132352386+nitinnj@users.noreply.github.com>
2025-12-27 01:20:20 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,3 +1,4 @@
 def call(String imageName, String imageVersion) {
-    sh "docker build -t ${imageName}:${imageVersion} ."
+    sh "docker build -t ${env.DOCKERHUB_USER}/${imageName}:${imageVersion} ."
 }
+
